53f7d00d3c701c6b658e7b397172b4957a1e781e
niclast7611 <niclast7611@gmail.com>
2022-10-06 16:43:57 -0700

    passed api key as query params both work

--- pages/charts/Test.js
+++ pages/charts/Test.js
@@ -3,34 +3,23 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 export default function Test() {
-  const [colorData, setColorData] = useState();
-  const [winsAndLosses, setWinsAndLosses] = useState();
+  const [colorData, setColorData] = useState([]);
+  const [winsAndLosses, setWinsAndLosses] = useState([]);
+  const [final, setFinal] = useState([]);
 
   useEffect(() => {
-    // **************
-    // const endpoints = [
-    //   `https://api.sportsdata.io/v3/mlb/scores/json/teams?key=${process.env.MLB_KEY}`,
-    //   `https://api.sportsdata.io/v3/mlb/scores/json/TeamSeasonStats/2022?key=${process.env.MLB_KEY}`,
-    // ];
-    // axios
-    //   .all(endpoints.map((endpoint) => axios.get(endpoint)))
-    // .then((response) => setSportsData(response));
-    // **************
-    // .then((response) => {
-    //   const filteredResponse = response.data.map((stats) => {
-    //     return {
-    //       wins: stats.Wins,
-    //       name: stats.Name,
-    //       losses: stats.Losses,
-    //     };
-    //   });
-    // **************
-
-    let one = `https://api.sportsdata.io/v3/mlb/scores/json/teams?key=${process.env.MLB_KEY}`;
-    let two = `https://api.sportsdata.io/v3/mlb/scores/json/TeamSeasonStats/2022?key=${process.env.MLB_KEY}`;
-
-    const requestOne = axios.get(one);
-    const requestTwo = axios.get(two);
+    const requestOne = axios.get(
+      "https://api.sportsdata.io/v3/mlb/scores/json/teams",
+      {
+        params: { key: process.env.MLB_KEY },
+      }
+    );
+    const requestTwo = axios.get(
+      "https://api.sportsdata.io/v3/mlb/scores/json/Standings/2022",
+      {
+        params: { key: process.env.MLB_KEY },
+      }
+    );
 
     axios
       .all([requestOne, requestTwo])
@@ -39,18 +28,36 @@ export default function Test() {
           const responseOne = responses[0].data;
           const responseTwo = responses[1].data.map((stats) => {
             return {
-              wins: stats.Wins,
-              name: stats.Name,
+              city: stats.City,
+              division: stats.Division,
+              divisionRank: stats.DivisionRank,
+              league: stats.League,
+              leagueRank: stats.LeagueRank,
               losses: stats.Losses,
+              name: stats.Name,
+              teamID: stats.TeamID,
+              wins: stats.Wins,
             };
           });
-          setColorData(responseOne);
-          setWinsAndLosses(responseTwo);
+          setColorData(responseOne.sort((a, b) => a.TeamID - b.TeamID));
+          setWinsAndLosses(responseTwo.sort((a, b) => a.teamID - b.teamID));
         })
       )
       .catch((errors) => {
         console.log(errors);
       });
+    finalObject();
   }, []);
+
+  const finalObject = () => {
+    const final = winsAndLosses.map((team, index) => {
+      return {
+        ...team,
+        primaryColor: colorData[index].PrimaryColor,
+        secondaryColor: colorData[index].SecondaryColor,
+      };
+    });
+    setFinal(final);
+  };
   return <div>Test</div>;
 }
