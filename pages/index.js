8437c6822c87e5eae982cb783108f574f884a98c
niclast7611 <niclast7611@gmail.com>
2022-10-13 15:21:06 -0700

    add tailwind and styled landing page

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,51 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <h1>HighChart Examples</h1>
-      <Link href="/charts/PageOne">
-        <button type="button">Page One</button>
-      </Link>
-      <Link href="/charts/PageTwo">
-        <button type="button">Page Two</button>
-      </Link>
-      <Link href="/charts/Test">
-        <button type="button">Test</button>
-      </Link>
+      <div class="py-48 mx-auto max-w-7xl px-4 sm:pt-24 md:pt-72 text-center">
+        <h1 class="font-extrabold text-black">
+          <p class="text-black bg-clip-text text-4xl sm;text-6xl md:text-7xl mb-5">
+            HighChart Examples
+          </p>
+        </h1>
+        <div class="mt-5 max-w-content mx-auto flex justify-center items-center md:mt-8">
+          <Link href="/charts/PageOne">
+            <button
+              type="button"
+              class="inline-flex items-center justify-center h-12 mr-6 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+            >
+              Browser DrillDown Chart
+            </button>
+          </Link>
+          <Link href="/charts/PageTwo">
+            <button
+              type="button"
+              class="inline-flex items-center justify-center h-12 mr-6 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+            >
+              Hard Coded MLB Sunburst
+            </button>
+          </Link>
+          <Link href="/charts/Test">
+            <button
+              type="button"
+              class="inline-flex items-center justify-center h-12 mr-6 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+            >
+              MLB Sunburst w/ API
+            </button>
+          </Link>
+        </div>
+      </div>
     </div>
+    // <div className="index-container">
+    //   <h1 id="index-title">HighChart Examples</h1>
+    //   <Link href="/charts/PageOne">
+    //     <button type="button">Page One</button>
+    //   </Link>
+    //   <Link href="/charts/PageTwo">
+    //     <button type="button">Page Two</button>
+    //   </Link>
+    //   <Link href="/charts/Test">
+    //     <button type="button">Test</button>
+    //   </Link>
+    // </div>
   );
 }
